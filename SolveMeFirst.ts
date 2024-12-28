1863a40362a2eb4bc40f76e42c532c9bba0298c6
Farhan Maulana Yasykur <maulanayasykur@gmail.com>
2024-12-28 15:34:20 +0700

    solve me first challange

---
+++ SolveMeFirst.ts
@@ -0,0 +1,27 @@
+"use strict";
+
+import process from "node:process";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+let inputString: string = "";
+let inputLines: string[] = [];
+let currentLine: number = 0;
+process.stdin.on("data", function (inputStdin: string): void {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", function (): void {
+  inputLines = inputString.split("\n");
+  inputString = "";
+  main();
+});
+
+function readLine(): string {
+  return inputLines[currentLine++];
+}
+
+function main() {
+  let a: number = Number(readLine());
+  console.log(a + Number(readLine()));
+}
